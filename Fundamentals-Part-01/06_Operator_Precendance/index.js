b0835bc8a9d073071f31d8ae9d3f7efddc40abb3
Hrutik Sargar <106005765+HrutikSargar@users.noreply.github.com>
2024-02-19 23:52:29 +0530

    Add files via upload

---
+++ Fundamentals-Part-01/06_Operator_Precendance/index.js
@@ -0,0 +1,10 @@
+console.log(25 - 10 - 5);
+let x, y;
+
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+let num1 = 10;
+let num2 = 20;
+//Assignment Operator precendance (left to right)
+console.log((num1 + num2) / 2);
