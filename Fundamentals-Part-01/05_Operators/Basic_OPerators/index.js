3be04a748e269a07f2d89343c335ecd6b7fee33f
Hrutik Sargar <106005765+HrutikSargar@users.noreply.github.com>
2024-02-19 23:52:07 +0530

    Add files via upload

---
+++ Fundamentals-Part-01/05_Operators/Basic_OPerators/index.js
@@ -0,0 +1,28 @@
+//math Operators
+const now = 2024;
+const ageHrutik = now - 2000;
+
+console.log(ageHrutik);
+
+console.log(ageHrutik * 2, ageHrutik / 10, 2 ** 3);
+//2 ** 3 means 2 to the power of 3=2*2*2
+
+const fName = "Hrutik";
+const lName = "Sargar";
+console.log(fName + lName);
+
+//Assignnment Operators
+let x = 10 + 3;
+console.log(x);
+x += 10; //x=x+10;
+x *= 4; //x=x*4;
+x++; //x=x+1;
+x--; //x=x-1;
+
+//Comparison Operator
+const num1 = 1;
+const num2 = 2;
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
