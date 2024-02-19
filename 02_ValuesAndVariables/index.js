46cd2d57b1904e0e83a55517409c4f1e65f227aa
Hrutik Sargar <106005765+HrutikSargar@users.noreply.github.com>
2024-02-19 23:46:39 +0530

    Add files via upload

---
+++ 02_ValuesAndVariables/index.js
@@ -0,0 +1,24 @@
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+//Naming Standards
+let hrutikSargar = "HS";
+
+//NAMING CONVENTION
+
+let firstName = "hrutik";
+let myFirstJob = "Programmer";
+
+//Not Acceptable
+// let 32Name="Hrutik"
+
+//Do not start Variable name with upper case
+let Person = "hrutik";
+
+//You can use reserved words like this
+let _function = "fun 1";
+let $function = "fun 2";
+
+//name is also reserved word but it is allowed
+let name = "hrutik";
