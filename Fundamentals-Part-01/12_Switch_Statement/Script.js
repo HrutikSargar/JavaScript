52832b87a997f2ed6d760379dac922222cea9091
Hrutik Sargar <106005765+HrutikSargar@users.noreply.github.com>
2024-02-20 19:45:54 +0530

    Update Script.js

--- Fundamentals-Part-01/12_Switch_Statement/Script.js
+++ Fundamentals-Part-01/12_Switch_Statement/Script.js
@@ -11,7 +11,8 @@ switch (day) {
     console.log("Plan tuesday");
     break;
   case "friday":
-    log("Plan friday");
+  case "Saturday":
+    console.log("Plan friday && Plan Saturday");
     break;
   default:
     console.log("No Plan+");
