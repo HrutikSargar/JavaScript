07190c7a15a223700f9e722b0d61f68c9a3017b9
Hrutik Sargar <106005765+HrutikSargar@users.noreply.github.com>
2024-02-20 19:44:12 +0530

    Add files via upload

---
+++ Fundamentals-Part-01/12_Switch_Statement/Script.js
@@ -0,0 +1,18 @@
+const day = "monday";
+
+switch (day) {
+  case "monday":
+    console.log("Plan monday"); //executed
+    break;
+  case "Thursday":
+    log("Plan Thursday");
+    break;
+  case "tuesday":
+    console.log("Plan tuesday");
+    break;
+  case "friday":
+    log("Plan friday");
+    break;
+  default:
+    console.log("No Plan+");
+}
