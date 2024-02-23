b3bfffe76bff7d9b151e358d0acff44b3a8fc88b
Hrutik Sargar <106005765+HrutikSargar@users.noreply.github.com>
2024-02-23 22:26:20 +0530

    Add files via upload

---
+++ Fundamentals-Part-02/01_Function/Script.js
@@ -0,0 +1,24 @@
+// Simple function which just log to the console.
+function logger() {
+  console.log("My name is Hrutik");
+}
+
+//Calling / running / invoking function
+
+logger();
+logger();
+logger();
+
+//Another Function which returns some value.
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+
+  return juice;
+}
+
+//Calling / running / invoking function
+
+console.log(fruitProcessor(2, 3));
+
+const appleOrangeJuice = fruitProcessor(4, 9);
+console.log(appleOrangeJuice);
